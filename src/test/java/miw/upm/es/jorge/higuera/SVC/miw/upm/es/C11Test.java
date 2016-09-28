5bd3b7e322ee535c35cf7297a5c15e8ec57633a0
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-09-28 21:59:35 +0200

    C11Test

---
+++ src/test/java/miw/upm/es/jorge/higuera/SVC/miw/upm/es/C11Test.java
@@ -0,0 +1,27 @@
+package miw.upm.es.jorge.higuera.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C11Test {
+	
+	private C11 c;
+	
+	@Before
+	public void before(){
+		c = new C11();
+	}
+
+	@Test
+	public void testM1() {
+		assertEquals("m1", c.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", c.m2());
+	}
+
+}
