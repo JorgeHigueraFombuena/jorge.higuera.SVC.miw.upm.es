c8bb7abaf9668ad503a9bdf2d1766288a4c17ffa
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-09-28 22:06:23 +0200

    C31Test

---
+++ src/test/java/miw/upm/es/jorge/higuera/SVC/miw/upm/es/C31Test.java
@@ -0,0 +1,27 @@
+package miw.upm.es.jorge.higuera.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C31Test {
+
+	private C31 c;
+	
+	@Before
+	public void before(){
+		c = new C31();
+	}
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1", c.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", c.m2());
+	}
+
+}
